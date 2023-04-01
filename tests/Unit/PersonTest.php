8be3a4da86acfec04d76ef3c5128fa11cdc14c63
Akemi Adam <90357785+akemi-adam@users.noreply.github.com>
2023-04-01 09:47:20 -0300

    Correção no último teste

--- tests/Unit/PersonTest.php
+++ tests/Unit/PersonTest.php
@@ -55,4 +55,4 @@ it('returns a random telephone', function ()
         ->toBeString()
         ->toContain('-', '(', ')')
         ->toMatch('/^(?:(?:\+|00)?(55)\s?)?(?:\(?([0-0]?[0-9]{1}[0-9]{1})\)?\s?)??(?:((?:9\d|[2-9])\d{3}\-?\d{4}))$/');
-})->only();
+});
